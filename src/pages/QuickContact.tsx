48d6a772c43b7e52ed27439a1fdb8aedf781e56b
Joey Weidman <pcjoe3343@gmail.com>
2022-03-29 15:43:48 -0600

    Added quick contact page.

--- src/pages/QuickContact.tsx
+++ src/pages/QuickContact.tsx
@@ -1,14 +1,42 @@
 import React from 'react';
 import logo from '../assets/logo.png'
 import Styled from 'styled-components';
+import { COLORS } from '../constants/colors';
 
 function QuickContact() {
     const HeaderContainer = Styled.div`
         display: flex;
-        height: 200px;
         position: relative;
         justify-content: center;
         align-items: center;
+        margin-top: 50px;
+    `;
+
+    const ButtonContainer = Styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin-top: 100px;
+    `;
+
+    const Button = Styled.a`
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: ${COLORS.primary};
+        margin: 15px;
+        height: 75px;
+        width: 150px;
+        border-radius: 10px;
+        font-size: 2.5rem;
+        text-decoration: none;
+        color: white;
+        border-width: 2px;
+        border-color: black;
+        border-style: solid;
+        cursor: default;
+        user-select: none;
     `;
 
     return (
@@ -16,6 +44,11 @@ function QuickContact() {
             <HeaderContainer>
                 <img src={logo} style={{width: '90%', height: 'auto'}}></img>
             </HeaderContainer>
+            <ButtonContainer>
+                <Button href="mailto:handemanpc@gmail.com?Subject=Test Subject&body=Test Message">Email</Button>
+                <Button href="tel:4355650680">Call</Button>
+                <Button href="sms:4355650680">Text</Button>
+            </ButtonContainer>
         </>
     );
 }
